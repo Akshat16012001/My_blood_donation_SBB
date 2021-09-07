301a2e3bc1fab5a74a7bc1fa13446cba4435b223
Akshat Singh <63160780+Akshat16012001@users.noreply.github.com>
2021-09-07 13:20:51 +0530

    first commit

---
+++ app/src/main/java/com/example/sbb/Model/Myresponce.java
@@ -0,0 +1,12 @@
+package com.example.sbb.Model;
+
+import java.util.List;
+
+
+public class Myresponce {
+    public long multicust_id;
+    public int success;
+    public int failar;
+    public int cononical_ids;
+    public List<Result> results;
+}
